0a99adcc24f2e37d7c43ea1acedfde4e60967981
Sergey <mrsergey2005@yandex.ru>
2023-05-31 22:08:59 +0300

    init commit

---
+++ todos/static/js/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    let button = document.getElementById("myButton");
+    let countSpan = document.getElementById("count");
+    let count = 0;
+
+    button.addEventListener("click", function() {
+        count++;
+        countSpan.innerText = count.toString();
+    });
+};
