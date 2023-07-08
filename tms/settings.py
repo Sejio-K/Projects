7aff3d39c24093ce23d4b3b2a25d662b47eca2df
Sergey <mrsergey2005@yandex.ru>
2023-07-08 22:52:57 +0300

    не получается отправить форму через Postman.

--- tms/settings.py
+++ tms/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n$+tqlhyg3060t=-mku-^d!3r68wx57b4b1ldo$z2pxb#uah-j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.10']
 
 
 # Application definition
--- todos/views.py
+++ todos/views.py
@@ -1,15 +1,27 @@
-import requests
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.views import View
-import json
+from django.views.decorators.csrf import csrf_exempt
+from .forms import TodoForm
 from .models import Todo
+from django.forms.models import model_to_dict
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
+@csrf_exempt
 def todos(request):
-    todos = Todo.objects.all()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = request.user  # Присваиваем текущего пользователя
+            todo.save()
+            return JsonResponse({'todo': model_to_dict(todo)})
+        else:
+            return JsonResponse({'status': 400, 'errors': form.errors})
+    todos = [model_to_dict(todos) for todos in Todo.objects.all()]
     return render(request, 'todos.html', {'todos': todos})
+
+
 def view_todo(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     return render(request, 'todo.html', {'todo': todo})
-
