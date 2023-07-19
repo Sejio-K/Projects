1e2ce1de9ab4c567dd8bdc9aed4a003df145da56
Sergey <mrsergey2005@yandex.ru>
2023-07-19 09:00:38 +0300

    init

--- todos/urls/todos.py
+++ todos/urls/todos.py
@@ -4,6 +4,6 @@ from todos.views.todos import todos, create_todo, update_todo, delete_todo
 urlpatterns = [
     path('', todos, name='todos'),
     path('create/', create_todo, name="create_todo"),
-    path('update/<int:todo_id>/ ', update_todo, name="update_todo")
+    path('update/<int:todo_id>/ ', update_todo, name="update_todo"),
     path('delete/<int:todo_id>/ ', delete_todo, name="delete_todo")
 ]
